travel000/fmt

commit 81c521a9bcbe6095503712513232942a79639b81

Add pass: EchoToPrint

src/Additionals/EchoToPrint.php
@@ -1,0 +1,74 @@
+<?php
+# Copyright (c) 2015, phpfmt and its authors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+final class EchoToPrint extends AdditionalPass {
+	public function candidate($source, $foundTokens) {
+		if (isset($foundTokens[T_ECHO])) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public function format($source) {
+		$this->tkns = token_get_all($source);
+		while (list($index, $token) = each($this->tkns)) {
+			list($id) = $this->getToken($token);
+			$this->ptr = $index;
+
+			if (T_ECHO == $id) {
+				$start = $index;
+				$end = $this->walkUsefulRightUntil($this->tkns, $index, [ST_SEMI_COLON, T_CLOSE_TAG]);
+				$convert = true;
+				for ($i = $start; $i < $end; $i++) {
+					$tkn = $this->tkns[$i];
+					if (ST_PARENTHESES_OPEN === $tkn[0]) {
+						$this->refWalkBlock($tkns, $ptr, ST_PARENTHESES_OPEN, ST_PARENTHESES_CLOSE);
+					} elseif (ST_BRACKET_OPEN === $tkn[0]) {
+						$this->refWalkBlock($tkns, $ptr, ST_BRACKET_OPEN, ST_BRACKET_CLOSE);
+					} elseif (ST_COMMA === $tkn[0]) {
+						$convert = false;
+						break;
+					}
+				}
+				if ($convert) {
+					$this->tkns[$start] = [T_PRINT, 'print'];
+				}
+			}
+		}
+
+		return $this->render();
+	}
+
+	/**
+	 * @codeCoverageIgnore
+	 */
+	public function getDescription() {
+		return 'Convert from T_ECHO to print.';
+	}
+
+	/**
+	 * @codeCoverageIgnore
+	 */
+	public function getExample() {
+		return <<<'EOT'
+<?php
+echo 1;
+
+print 2;
+?>
+EOT;
+	}
+
+}

src/Core/FormatterPass.php
@@ -906,6 +906,20 @@ abstract class FormatterPass {
 		}
 	}
 
+	protected function walkUsefulRightUntil($tkns, $idx, $tokens) {
+		$ignoreList = $this->resolveIgnoreList($this->ignoreFutileTokens);
+		$tokens = array_flip($tokens);
+
+		while ($idx > 0 && isset($tkns[$idx])) {
+			$idx = $this->walkRight($tkns, $idx, $ignoreList);
+			if (isset($tokens[$tkns[$idx][0]])) {
+				return $idx;
+			}
+		}
+
+		return;
+	}
+
 	private function calculateCacheKey($direction, $ignoreList) {
 		return $direction . "\x2" . implode('', $ignoreList);
 	}

fmt-external.phar.sha1
@@ -1,1 +1,1 @@
-95d31d1ea60a1d063d0ef0394b4618dc51fe1463
+0c538924e7d59ca1543430783d110a4e83f6fe4e

fmt-external.php
@@ -2694,6 +2694,20 @@ abstract class FormatterPass {
 		}
 	}
 
+	protected function walkUsefulRightUntil($tkns, $idx, $tokens) {
+		$ignoreList = $this->resolveIgnoreList($this->ignoreFutileTokens);
+		$tokens = array_flip($tokens);
+
+		while ($idx > 0 && isset($tkns[$idx])) {
+			$idx = $this->walkRight($tkns, $idx, $ignoreList);
+			if (isset($tokens[$tkns[$idx][0]])) {
+				return $idx;
+			}
+		}
+
+		return;
+	}
+
 	private function calculateCacheKey($direction, $ignoreList) {
 		return $direction . "\x2" . implode('', $ignoreList);
 	}
@@ -2876,6 +2890,8 @@ abstract class BaseCodeFormatter {
 		'PSR1OpenTags' => false,
 		'PHPDocTypesToFunctionTypehint' => false,
 		'RemoveSemicolonAfterCurly' => false,
+		'NewLineBeforeReturn' => false,
+		'EchoToPrint' => false,
 	];
 
 	private $hasAfterExecutedPass = false;
@@ -2925,9 +2941,9 @@ abstract class BaseCodeFormatter {
 		$this->passes['NormalizeLnAndLtrimLines'] = new NormalizeLnAndLtrimLines();
 		$this->passes['OrderAndRemoveUseClauses'] = new OrderAndRemoveUseClauses();
 		$this->passes['Reindent'] = new Reindent();
+		$this->passes['ReindentColonBlocks'] = new ReindentColonBlocks();
 		$this->passes['ReindentComments'] = new ReindentComments();
 		$this->passes['ReindentEqual'] = new ReindentEqual();
-		$this->passes['ReindentColonBlocks'] = new ReindentColonBlocks();
 		$this->passes['ReindentObjOps'] = new ReindentObjOps();
 		$this->passes['RemoveIncludeParentheses'] = new RemoveIncludeParentheses();
 		$this->passes['ResizeSpaces'] = new ResizeSpaces();
@@ -2935,6 +2951,7 @@ abstract class BaseCodeFormatter {
 		$this->passes['SplitCurlyCloseAndTokens'] = new SplitCurlyCloseAndTokens();
 		$this->passes['StripExtraCommaInList'] = new StripExtraCommaInList();
 		$this->passes['TwoCommandsInSameLine'] = new TwoCommandsInSameLine();
+
 		$this->hasAfterExecutedPass = method_exists($this, 'afterExecutedPass');
 		$this->hasAfterFormat = method_exists($this, 'afterFormat');
 		$this->hasBeforePass = method_exists($this, 'beforePass');

fmt.phar.sha1
@@ -1,1 +1,1 @@
-bf9e99c9604e33080499551aae93376cd2fa0d06
+2523842f7a675fc8e003c7096f970c36aa660364

fmt.php
@@ -2694,6 +2694,20 @@ abstract class FormatterPass {
 		}
 	}
 
+	protected function walkUsefulRightUntil($tkns, $idx, $tokens) {
+		$ignoreList = $this->resolveIgnoreList($this->ignoreFutileTokens);
+		$tokens = array_flip($tokens);
+
+		while ($idx > 0 && isset($tkns[$idx])) {
+			$idx = $this->walkRight($tkns, $idx, $ignoreList);
+			if (isset($tokens[$tkns[$idx][0]])) {
+				return $idx;
+			}
+		}
+
+		return;
+	}
+
 	private function calculateCacheKey($direction, $ignoreList) {
 		return $direction . "\x2" . implode('', $ignoreList);
 	}
@@ -2876,6 +2890,8 @@ abstract class BaseCodeFormatter {
 		'PSR1OpenTags' => false,
 		'PHPDocTypesToFunctionTypehint' => false,
 		'RemoveSemicolonAfterCurly' => false,
+		'NewLineBeforeReturn' => false,
+		'EchoToPrint' => false,
 	];
 
 	private $hasAfterExecutedPass = false;
@@ -2925,9 +2941,9 @@ abstract class BaseCodeFormatter {
 		$this->passes['NormalizeLnAndLtrimLines'] = new NormalizeLnAndLtrimLines();
 		$this->passes['OrderAndRemoveUseClauses'] = new OrderAndRemoveUseClauses();
 		$this->passes['Reindent'] = new Reindent();
+		$this->passes['ReindentColonBlocks'] = new ReindentColonBlocks();
 		$this->passes['ReindentComments'] = new ReindentComments();
 		$this->passes['ReindentEqual'] = new ReindentEqual();
-		$this->passes['ReindentColonBlocks'] = new ReindentColonBlocks();
 		$this->passes['ReindentObjOps'] = new ReindentObjOps();
 		$this->passes['RemoveIncludeParentheses'] = new RemoveIncludeParentheses();
 		$this->passes['ResizeSpaces'] = new ResizeSpaces();
@@ -2935,6 +2951,7 @@ abstract class BaseCodeFormatter {
 		$this->passes['SplitCurlyCloseAndTokens'] = new SplitCurlyCloseAndTokens();
 		$this->passes['StripExtraCommaInList'] = new StripExtraCommaInList();
 		$this->passes['TwoCommandsInSameLine'] = new TwoCommandsInSameLine();
+
 		$this->hasAfterExecutedPass = method_exists($this, 'afterExecutedPass');
 		$this->hasAfterFormat = method_exists($this, 'afterFormat');
 		$this->hasBeforePass = method_exists($this, 'beforePass');
@@ -8576,6 +8593,63 @@ EOT;
 }
 
 	
+final class EchoToPrint extends AdditionalPass {
+	public function candidate($source, $foundTokens) {
+		if (isset($foundTokens[T_ECHO])) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public function format($source) {
+		$this->tkns = token_get_all($source);
+		while (list($index, $token) = each($this->tkns)) {
+			list($id) = $this->getToken($token);
+			$this->ptr = $index;
+
+			if (T_ECHO == $id) {
+				$start = $index;
+				$end = $this->walkUsefulRightUntil($this->tkns, $index, [ST_SEMI_COLON, T_CLOSE_TAG]);
+				$convert = true;
+				for ($i = $start; $i < $end; $i++) {
+					$tkn = $this->tkns[$i];
+					if (ST_PARENTHESES_OPEN === $tkn[0]) {
+						$this->refWalkBlock($tkns, $ptr, ST_PARENTHESES_OPEN, ST_PARENTHESES_CLOSE);
+					} elseif (ST_BRACKET_OPEN === $tkn[0]) {
+						$this->refWalkBlock($tkns, $ptr, ST_BRACKET_OPEN, ST_BRACKET_CLOSE);
+					} elseif (ST_COMMA === $tkn[0]) {
+						$convert = false;
+						break;
+					}
+				}
+				if ($convert) {
+					$this->tkns[$start] = [T_PRINT, 'print'];
+				}
+			}
+		}
+
+		return $this->render();
+	}
+
+	
+	public function getDescription() {
+		return 'Convert from T_ECHO to print.';
+	}
+
+	
+	public function getExample() {
+		return <<<'EOT'
+<?php
+echo 1;
+
+print 2;
+?>
+EOT;
+	}
+
+}
+	
 final class EncapsulateNamespaces extends AdditionalPass {
 	public function candidate($source, $foundTokens) {
 		if (isset($foundTokens[T_NAMESPACE])) {
@@ -9258,6 +9332,61 @@ final class MildAutoPreincrement extends AutoPreincrement {
 		return 'Automatically convert postincrement to preincrement. (Deprecated pass. Use AutoPreincrement instead).';
 	}
 }
+	
+final class NewLineBeforeReturn extends AdditionalPass {
+	public function candidate($source, $foundTokens) {
+		if (isset($foundTokens[T_RETURN])) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public function format($source) {
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->getToken($token);
+			$this->ptr = $index;
+			switch ($id) {
+			case T_RETURN:
+				$this->rtrimAndAppendCode($this->newLine . $this->newLine . $text);
+				break;
+			default:
+				$this->appendCode($text);
+				break;
+			}
+		}
+
+		return $this->code;
+	}
+
+	
+	public function getDescription() {
+		return 'Add an empty line before T_RETURN.';
+	}
+
+	
+	public function getExample() {
+		return <<<'EOT'
+<?php
+// From
+function a(){
+	$a = 1;
+	return $a;
+}
+
+// To
+function a(){
+	$a = 1;
+
+	return $a;
+}
+?>
+EOT;
+	}
+}
+
 	
 final class NoSpaceAfterPHPDocBlocks extends FormatterPass {
 	public function candidate($source, $foundTokens) {

kodeclan/phpfmt

commit f184aada6f8d2bf1aed7af887ebc09e8e0cc4e71

Fix parse errors introduced with 81c521a9bcbe6095503712513232942a79639b81 and revealed with strict PHP enforcement in 51d48a2fe0c0664a837dee54b9eace6ef755055c

src/Additionals/EchoToPrint.php
@@ -34,9 +34,9 @@ final class EchoToPrint extends AdditionalPass {
 				for ($i = $start; $i < $end; $i++) {
 					$tkn = $this->tkns[$i];
 					if (ST_PARENTHESES_OPEN === $tkn[0]) {
-						$this->refWalkBlock($tkns, $ptr, ST_PARENTHESES_OPEN, ST_PARENTHESES_CLOSE);
+						$this->refWalkBlock($this->tkns, $this->ptr, ST_PARENTHESES_OPEN, ST_PARENTHESES_CLOSE);
 					} elseif (ST_BRACKET_OPEN === $tkn[0]) {
-						$this->refWalkBlock($tkns, $ptr, ST_BRACKET_OPEN, ST_BRACKET_CLOSE);
+						$this->refWalkBlock($this->tkns, $this->ptr, ST_BRACKET_OPEN, ST_BRACKET_CLOSE);
 					} elseif (ST_COMMA === $tkn[0]) {
 						$convert = false;
 						break;

src/Core/FormatterPass.php
@@ -919,7 +919,7 @@ abstract class FormatterPass {
 			}
 		}
 
-		return;
+		return count($tkns);
 	}
 
 	private function calculateCacheKey(string $direction, array $ignoreList): string {